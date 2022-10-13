dc418450525dbe84bf988f72e7232a2ab8942832
Nurul Farah Hani binti Zaimal <nurul.farah.zaimal@accenture.com>
2022-10-13 15:11:49 +0800

    Changed index page

---
+++ component/login/loginFrom.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './loginForm.module.scss'
+
+export default function LoginForm() {
+    return (
+        <div className={styles.centerLogo}>
+            <Image src="/assets/images/RMS.png" alt="logo" width={500} height={100} />
+        </div>
+    )
+}
