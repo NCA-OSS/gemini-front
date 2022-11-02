d20eba8bad22ba45ec9c3161e82fcb10d151e44c
Hong Shen <hongshen1122@gmail.com>
2022-11-02 17:18:16 +0800

    migrated the css of navbar from Global to navbar.module

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 //Component
 import LoginForm from '../component/login/loginFrom'
 import Navbar from '../component/shared/navbar/navBar';
+import Table from '../component/shared/dashboard/dashboard';
 
 //Style
 import styles from './index.module.scss';
@@ -15,6 +16,7 @@ export default function Home() {
     <div className={styles.loginBackground}>
       <LoginForm />
       <Navbar />
+      <Table />
     </div>
   )
 };
