6c2c18e6997b140daaac717a689b9655c8062441
shen1212 <110951166+shen1212@users.noreply.github.com>
2022-11-02 17:24:08 +0800

    Revert "Navbar"

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import LoginForm from '../component/login/loginFrom'
-import Navbar from '../component/shared/navbar/navBar';
-import Table from '../component/shared/dashboard/dashboard';
 
 export default function Home() {
-
-    return (
-    <>
-
-        <Table />
-
-    </>
-  );
-};
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
+}
